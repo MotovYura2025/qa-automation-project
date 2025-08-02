471722c94496d122bacd511cfb45bc7401725b26
Yura <marlboro_2026@mail.ru>
2025-08-02 21:33:29 +1000

    Initial commit - добавлены файлы из папки Для Git hub

--- all my projects/9/qa-base-lesson-9-yarygin-main/src/test/java/BaseTest.java
+++
@@ -1,9 +0,0 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-
-public class BaseTest {
-    @BeforeAll
-    public static void setUp() {
-        Configuration.browser = "firefox";
-    }
-}
